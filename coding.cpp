2b30e1bfd9b263dd3f6b25bb118c77a8d27383c4
dragon-air <sourabhkr.pasi@gmail.com>
2017-05-21 17:09:21 +0530

    snackdown qualifier

--- coding.cpp
+++ coding.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define el cout<<endl;
 
 vector<ll> arr;
 multiset<ll> mset;
@@ -19,7 +20,7 @@ void solve(void)
 
    arr.clear();
    mset.clear();
-   int n,q;
+   int n,q,ans=0;
    cin>>n>>q;
    for(int i=0;i<n;i++ )
      {
@@ -28,18 +29,57 @@ mset.insert(val);
    }
 
 
-   print();
+  //print();
 
 multiset<long long>:: iterator it1,it2;
 it2=mset.begin();
 
 while(q-- ){
-cout<<"gfreg"<<endl;
+ans=0;
 cin>>val;
-it1 = mset.upper_bound(val);
-cout<<*it1;
+it1=mset.lower_bound(val);
+//cout<<*it1;
+//el
+if(it1!=mset.begin()){
+   // it1=it1-1;
+    //prev(it1,1);
 
-cout<<distance(it1,it2);
+    auto pos =distance(it1,mset.end());
+    ans = pos;
+  //  cout<<ans;
+    //el
+    it1--;
+    //cout<<"prev   "<<*it1;
+    //el
+    pos = distance(mset.begin(),it1);
+    int avail = pos;
+    int req = val-*it1;
+    //cout<<"avail  "<<avail;
+    //el
+    //cout<<"req    "<<req;
+    //el
+    while(avail>=req && it1!=mset.begin() && avail>=1){
+        ans++;
+        avail = avail - req;
+         it1--;
+          req = val - *it1;
+      //  cout<<"avail  "<<avail;
+        //el
+       // cout<<"req  "<<req;
+         //el
+        //cout<<"it1  "<<*it1;
+      // el
+    }
+}
+
+cout<<ans;
+el
+//auto pos = distance(mset.begin(), it1);
+
+//cout<<pos;
+
+
+
 }
 
 
